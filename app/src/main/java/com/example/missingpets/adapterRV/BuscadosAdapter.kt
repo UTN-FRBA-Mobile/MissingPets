cea5230438976ba12a020e18581bde8a349e6d4f
IngScyther <IngScyther@users.noreply.github.com>
2021-10-10 14:09:59 -0300

    Agrego estructura del adapter para el RV

--- app/src/main/java/com/example/missingpets/adapterRV/BuscadosAdapter.kt
+++ app/src/main/java/com/example/missingpets/adapterRV/BuscadosAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.missingpets.adapterRV
 
-class BuscadosAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.missingpets.modelRV.Buscado
+
+class BuscadosAdapter (private val context: Context, private val dataset: List<Buscado>)
+    : RecyclerView.Adapter<BuscadosAdapter.BuscadoViewHolder>() {
+
+    class BuscadoViewHolder (private val view: View) : RecyclerView.ViewHolder(view) {
+        
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BuscadosAdapter.BuscadoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BuscadosAdapter.BuscadoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
