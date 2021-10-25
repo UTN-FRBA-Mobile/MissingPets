c29f2822f7d580d54be6f20a066208fb2d50292a
Unknown <matiasblacutt16@gmail.com>
2021-10-25 01:06:54 -0300

    Add navigation from Register to Login and add validation empty fields in RegisterFragment

--- app/src/main/java/com/example/missingpets/RegisterFragment.kt
+++ app/src/main/java/com/example/missingpets/RegisterFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.missingpets.viewModels.RegisterViewModel
+import com.example.missingpets.databinding.FragmentRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +26,10 @@ class RegisterFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var viewModel: RegisterViewModel
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +42,66 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = RegisterViewModel()
+
+
+        binding.btnRegistrarse.setOnClickListener {
+            val username = getUsername()
+            val email = getEmail()
+            val contrasenia = getContrasenia()
+            if (viewModel.camposEstanVacios(username, email, contrasenia)){
+                notificarCamposVacios(view)
+            }else{
+                viewModel.registrar(username, email, contrasenia)
+            }
+        }
+        binding.tvIniciarSesion.setOnClickListener {
+            val action = R.id.action_registerFragment_to_loginFragment2
+            findNavController().navigate(action)
+        }
+
+
+    }
+
+    fun getUsername(): String?{
+        return binding.etNombreUsuarioNuevo.text.toString()
+    }
+
+    fun getEmail(): String?{
+        return binding.etEmailUsuarioNuevo.text.toString()
+    }
+
+    fun getContrasenia(): String?{
+        return binding.etContraseniaUsuarioNuevo.text.toString()
+    }
+
+    fun notificarCamposVacios(view: View){
+        //val toast = Toast.makeText(context, "Complete todos los campos", Toast.LENGTH_SHORT)
+        //toast.show()
+        Snackbar.make(view, "Complete todos los campos", Snackbar.LENGTH_LONG)
+            .show();
+    }
+
+    fun registroExitoso(){
+        //TODO
+    }
+
+    fun registroFallido(view: View){
+        Snackbar.make(view, "Se perdio conexion con el servidor", Snackbar.LENGTH_LONG)
+            .show();
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
