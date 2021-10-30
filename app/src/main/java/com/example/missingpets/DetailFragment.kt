91bc5599ce23df79d60006092f5ec866d21c7ec1
BlacuttMatias <matiasblacutt16@gmail.com>
2021-10-30 14:16:26 -0300

    Initialize DetailViewModel with hardcoded values

--- app/src/main/java/com/example/missingpets/DetailFragment.kt
+++ app/src/main/java/com/example/missingpets/DetailFragment.kt
@@ -5,24 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import com.example.missingpets.databinding.FragmentDetailBinding
 import com.example.missingpets.databinding.FragmentRegisterBinding
 import com.example.missingpets.viewModels.DetailViewModel
 import com.example.missingpets.viewModels.RegisterViewModel
+import java.util.*
 
 class DetailFragment : Fragment() {
 
-    private lateinit var viewModel: DetailViewModel
+    private lateinit var detailViewModel: DetailViewModel
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
     }
 
     override fun onCreateView(
@@ -36,6 +35,31 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val nombreAnimal = getArgumentoNombreAnimal()
+        val tipoAnimal = getArgumentoTipoAnimal()
+        val sexoAnimal = getArgumentoSexoAnimal()
+        val fechaPerdido = getArgumentoFechaPerdido()
+
+        detailViewModel = DetailViewModel(nombreAnimal, tipoAnimal, sexoAnimal, fechaPerdido)
+
+    }
+
+    fun getArgumentoNombreAnimal(): String{
+        return "pelusa"
+    }
+    
+    fun getArgumentoTipoAnimal(): String{
+        return "gato"
+    }
+
+
+    fun getArgumentoSexoAnimal(): String{
+        return "masculino"
+    }
+
+
+    fun getArgumentoFechaPerdido(): Date{
+        return Date()
     }
 
 }
