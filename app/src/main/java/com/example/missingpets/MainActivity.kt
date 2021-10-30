dfc9552d6195315cb340000b7e13784e62f92cb7
asp2021 <danielfp@hotmail.com>
2021-10-29 22:49:36 -0300

    bottom_navigation

--- app/src/main/java/com/example/missingpets/MainActivity.kt
+++ app/src/main/java/com/example/missingpets/MainActivity.kt
@@ -2,14 +2,43 @@ package com.example.missingpets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.missingpets.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        val missingFragment = MissingFragment()
+        val foundFragment = FoundFragment()
+        val newPostFragment = NewPostFragment()
+
+        replaceFragment(missingFragment)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.id_missing -> replaceFragment(missingFragment)
+                R.id.id_found -> replaceFragment(foundFragment)
+                R.id.id_add_notification -> replaceFragment(newPostFragment)
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, fragment)
+            commit()
+        }
     }
 }
