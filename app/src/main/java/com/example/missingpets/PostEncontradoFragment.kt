d8a02e0cf3ed21cc2c76e606fa815b87cb741868
marumarino <marimarino@frba.utn.edu.ar>
2021-11-11 01:08:14 -0300

    agrego nuevo post animal encontrado

--- app/src/main/java/com/example/missingpets/PostEncontradoFragment.kt
+++ app/src/main/java/com/example/missingpets/PostEncontradoFragment.kt
@@ -1,10 +1,25 @@
 package com.example.missingpets
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.missingpets.databinding.FragmentPostEncontradoBinding
+import com.example.missingpets.databinding.FragmentPostMissingBinding
+import com.google.android.material.snackbar.Snackbar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,12 +32,34 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PostEncontradoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentPostEncontradoBinding? = null
+    private val binding get() = _binding!!
+
+
+    private var resultLauncherCamara = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            val extras = data?.extras
+            val imgBitmap = extras!!["data"] as Bitmap?
+            binding.ivMascotaEncontrada.setImageBitmap(imgBitmap)
+
+        }
+    }
+    private var resultLauncherGaleria = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val picture = result.data?.data
+            binding.ivMascotaEncontrada.setImageURI(picture)
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -32,28 +69,102 @@ class PostEncontradoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_encontrado, container, false)
+        _binding = FragmentPostEncontradoBinding.inflate(inflater, container, false)
+
+        //Spinner del tipo del animal
+        val tipoDeAnimal = resources.getStringArray(R.array.animals)
+        val spinnerAnimales = binding.spnTipoAnimales
+        // initialize an array adapter for spinner
+        val adapterAnimales: ArrayAdapter<String> = initializeSpinnerAdapter(tipoDeAnimal, spinnerAnimales)
+        // finally, data bind spinner with adapter
+        spinnerAnimales.adapter = adapterAnimales
+
+        // Spinner del sexo del animal
+        val sexoDelAnimal = resources.getStringArray(R.array.animals_sex)
+        val spinnerSexo = binding.spnSexoAnimales
+        // initialize an array adapter for spinner
+        val adapter: ArrayAdapter<String> = initializeSpinnerAdapter(sexoDelAnimal, spinnerSexo)
+        // finally, data bind spinner with adapter
+        spinnerSexo.adapter = adapter
+
+
+
+        return binding.root
+        //inflater.inflate(R.layout.fragment_post_missing, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PostAdoptionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PostEncontradoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnTomarFotoEncontrado.setOnClickListener {
+            abrirCamara(view)
+        }
+        binding.btnSubirFotoEncontrado.setOnClickListener {
+            abrirGaleria(view)
+        }
+    }
+
+    private fun abrirGaleria(view: View) {
+        try {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            //startActivity(intent)
+            intent.type = "image/*"
+            resultLauncherGaleria.launch(intent)
+
+        } catch (e: Exception) {
+            Snackbar.make(view, "No se pudo abrir la galeria", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    // Abrir la cámara o subir desde la galeria
+    private fun abrirCamara(view: View) {
+        try {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (activity?.let { intent.resolveActivity(it.packageManager) } != null) {
+                //startActivity(intent)
+                resultLauncherCamara.launch(intent)
+            } else {
+                Toast.makeText(this.context, "No se encontró cámara", Toast.LENGTH_SHORT).show()
             }
+
+        } catch (e: Exception) {
+            Snackbar.make(view, "No se pudo abrir cámara", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    private fun initializeSpinnerAdapter(items: Array<String>, spinner: Spinner): ArrayAdapter<String> {
+
+        return object : ArrayAdapter<String>( this.requireContext(),
+            android.R.layout.simple_spinner_dropdown_item, items) {
+
+            override fun getDropDownView( position: Int, convertView: View?, parent: ViewGroup): View {
+                val view: TextView = super.getDropDownView(position, convertView, parent) as TextView
+                // set item text bold
+                view.setTypeface(view.typeface, Typeface.BOLD)
+
+                // set selected item style
+                if (position == spinner.selectedItemPosition && position != 0) {
+                    view.background = ColorDrawable(Color.parseColor("#F7E7CE"))
+                    view.setTextColor(Color.parseColor("#333399"))
+                }
+
+                // make hint item color gray
+                if (position == 0) {
+                    view.setTextColor(Color.LTGRAY)
+                }
+
+                return view
+            }
+
+            override fun isEnabled(position: Int): Boolean {
+                // disable first item
+                // first item is display as hint
+                return position != 0
+            }
+        }
     }
 }
