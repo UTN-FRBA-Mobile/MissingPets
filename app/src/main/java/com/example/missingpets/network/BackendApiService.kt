19be675a4ae9cd8ba899dd0304f9c5fcf146ef53
Jose Fuentes <jofuentes@frba.utn.edu.ar>
2021-11-16 23:04:50 -0300

    Trae datos del servidor

--- app/src/main/java/com/example/missingpets/network/BackendApiService.kt
+++ app/src/main/java/com/example/missingpets/network/BackendApiService.kt
@@ -2,13 +2,22 @@ package com.example.missingpets.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Url
 
-private const val BASE_URL = "http://sea.net.ar/missingpets/api/"
+
+
+
+
+
+
+
+private const val BASE_URL = "https://sea.net.ar/missingpets/api/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -19,15 +28,27 @@ private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .build()
 
-
-interface BackendApiService {
+//Defino los endpoints
+public interface BackendApiService {
     @GET("photos")
     suspend fun getPhotos(): List<ApiData>
 
     @GET("lost")
     suspend fun getLost(): List<MissingPet>
+
+/*    @FormUrlEncoded
+    @DELETE("lost")
+    suspend fun deleteLost(@Query("id") absenceId: Int, @Url url: String?): Call<ServerResponse?>?
+ */
+
 }
 
 public fun getRetrofit(): Retrofit {
     return retrofit;
+}
+
+
+object BackendApi {
+    val retrofitService : BackendApiService by lazy {
+        retrofit.create(BackendApiService::class.java) }
 }
