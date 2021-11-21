f313143b4b9f4923075403a9c94aa47adf07af6c
IngScyther <IngScyther@users.noreply.github.com>
2021-11-21 16:57:22 -0300

    Fix OptionMenu

--- app/src/main/java/com/example/missingpets/MainActivity.kt
+++ app/src/main/java/com/example/missingpets/MainActivity.kt
@@ -7,7 +7,11 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.example.missingpets.databinding.ActivityMainBinding
 import com.example.missingpets.network.ApiClient
@@ -29,22 +33,9 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment)  as NavHostFragment
         val navController = navHostFragment.navController
         botonNavegationView.setupWithNavController(navController)
-
-
     }
 
-    override fun onResume() {
-        super.onResume()
 
-      //  refreshSettingsPreferences()
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment, fragment)
-            commit()
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -52,37 +43,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val configurationFragment = ConfigurationFragment()
-        val aboutUsFragment = AboutUsFragment()
 
         when (item.itemId) {
-
-            R.id.action_settings -> replaceFragment(configurationFragment)
-            R.id.action_about -> replaceFragment(aboutUsFragment)
+            //R.id.action_logout -> //TODO(): Logica del Logout
         }
-
-        return super.onOptionsItemSelected(item)
+        val navController = findNavController(R.id.fragment)
+        return  item.onNavDestinationSelected(navController)|| super.onOptionsItemSelected(item)
     }
 
-    // desactivado falta inicializar para que no cuelgue el inicio //
-    private fun refreshSettingsPreferences() {
 
-        val imgSize = sharedPreferences.getString( getString(R.string.preferences_key_ui_img_size),"")
-        val sizeValue = when(imgSize){
-            getString(R.string.preferences_key_img_size_small) -> {
-                resources.getDimensionPixelSize(R.dimen.profile_image_size_small)
-            }
-            getString(R.string.preferences_key_img_size_medium) -> {
-                resources.getDimensionPixelSize(R.dimen.profile_image_size_medium)
-            }
-            else -> resources.getDimensionPixelSize(R.dimen.profile_image_size_large)
-        }
-        /* aca asigno las medidas a la imagen . Falta ver a que imagen ?
-        binding.item_image.updateLayoutParams {
-            width = sizeValue
-            height = sizeValue
-        } */
-  
-    }
 
 }
