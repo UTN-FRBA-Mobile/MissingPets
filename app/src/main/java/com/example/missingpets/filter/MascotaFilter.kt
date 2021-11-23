3d508b91079364d05bbf84b86a7a79380a694f9d
Jose Fuentes <jofuentes@frba.utn.edu.ar>
2021-11-23 09:36:13 -0300

    Filtros

--- app/src/main/java/com/example/missingpets/filter/MascotaFilter.kt
+++ app/src/main/java/com/example/missingpets/filter/MascotaFilter.kt
@@ -2,40 +2,41 @@ package com.example.missingpets.filter
 
 import com.example.missingpets.network.Mascota
 
+/**
+ * Este filtro recibe un List de Mascota, y filtra en base a un Map de String.  En el Map cada
+ * criterio de busqueda es un par key-value, y en el caso de la distancia maxima son 3 key-value: 1 para
+ * la distancia en kilometros, otro para la latitud, y otro para la longitud
+ */
+
 class MascotaFilter {
 
-    private fun filter (list: List<Mascota>, filter: Map<String?,String>): List<Mascota> {
+    private fun filter (list: List<Mascota>, searchCriteria: Map<String?,String>): List<Mascota> {
         var result: List<Mascota>
-        if(filter.containsKey("tipomascota")) {
-            list.filter {  }
+
+        //Filtro por tipomascota
+        if(searchCriteria.containsKey("tipomascota")) {
+            result = list.filter { it.tipoAnimal == searchCriteria.get("tipoanimal") }
+        } else {
+            result = list
         }
-        return list
+
+        //TODO agregar el resto de los filtros
+
+        if(searchCriteria.containsKey("distanciamaxima")) {
+            result = list.filter { calcularDistancia(it.latitude?.toDouble(), it.longitude?.toDouble(),
+                searchCriteria.get("latitude")?.toDouble(), searchCriteria.get("longitude")?.toDouble()) <= searchCriteria.get("distanciamaxima")?.toInt()!!
+            }
+        } else {
+            result = list
+        }
+
+        return result
     }
 
 
-    private fun calcularDistancia(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-        val theta = lon1 - lon2
-        var dist = (Math.sin(deg2rad(lat1))
-                * Math.sin(deg2rad(lat2))
-                + (Math.cos(deg2rad(lat1))
-                * Math.cos(deg2rad(lat2))
-                * Math.cos(deg2rad(theta))))
-        dist = Math.acos(dist)
-        dist = rad2deg(dist)
-        dist = dist * 60 * 1.1515
-        return dist
+    private fun calcularDistancia(lat1: Double?, lon1: Double?, lat2: Double?, lon2: Double?): Double {
+        //TODO calcular la distancia en kilómetros
+        return 0.0
     }
-
-    private fun deg2rad(deg: Double): Double {
-        return deg * Math.PI / 180.0
-    }
-
-    private fun rad2deg(rad: Double): Double {
-        return rad * 180.0 / Math.PI
-    }
-
-}
-
-private fun <E> List<E>.filter(predicate: (E) -> Unit) {
 
 }
