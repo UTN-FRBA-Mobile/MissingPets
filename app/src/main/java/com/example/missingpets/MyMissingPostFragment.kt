3d9c8ec703f8108f4b59e4faf0edb26579736074
IngScyther <IngScyther@users.noreply.github.com>
2021-11-25 18:51:01 -0300

    Update MyMissingPostFragment.kt
    Agrego advertencia cuando no tiene post cargados.

--- app/src/main/java/com/example/missingpets/MyMissingPostFragment.kt
+++ app/src/main/java/com/example/missingpets/MyMissingPostFragment.kt
@@ -1,6 +1,7 @@
 package com.example.missingpets
 
 import MissingAdapter
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -130,7 +132,19 @@ class MyMissingPostFragment : Fragment() {
 
                     } else {
 
-                        Toast.makeText(context, "No tienen post activos", Toast.LENGTH_SHORT).show()
+                        val alertDialog: AlertDialog? = activity?.let {
+                            val builder = AlertDialog.Builder(it)
+                            builder.apply {
+
+                                setTitle("Advertencia")
+                                setMessage("No tienes post cargados.")
+                                setPositiveButton("Aceptar",
+                                    DialogInterface.OnClickListener { dialog, id ->
+                                    })
+                            }
+                            builder.create()
+                        }
+                        alertDialog?.show()
                     }
 
                 }
