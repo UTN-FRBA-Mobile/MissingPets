fcc899322cb95b1e2bd48a7b2edd7c82cf0a3a29
Jose Fuentes <jofuentes@frba.utn.edu.ar>
2021-11-26 08:25:25 -0300

    Formato de fechas

--- app/src/main/java/com/example/missingpets/formats/DateFormat.java
+++ app/src/main/java/com/example/missingpets/formats/DateFormat.java
@@ -4,10 +4,18 @@ package com.example.missingpets.formats;
 
 public class DateFormat {
 
-    public static String yyyymmddToddmmyyy(String datestring) {
-        return datestring.substring(8, 10) + "-" + datestring.substring(5, 7) + "-" + datestring.substring(0, 4);
+    public static String yyyymmddToddmmyyyy(String datestring) {
+        if(datestring.substring(4, 5).equals("-")) {
+            return datestring.substring(8, 10) + "-" + datestring.substring(5, 7) + "-" + datestring.substring(0, 4);
+        } else {
+            return datestring;
+        }
     }
-    public static String ddmmyyyToyyyymmdd(String datestring) {
-        return datestring.substring(6, 10) + "-" + datestring.substring(3, 5) + "-" + datestring.substring(0, 2);
+    public static String ddmmyyyyToyyyymmdd(String datestring) {
+        if(datestring.substring(2, 3).equals("-")) {
+            return datestring.substring(6, 10) + "-" + datestring.substring(3, 5) + "-" + datestring.substring(0, 2);
+        } else {
+            return datestring;
+        }
     }
 }
--- app/src/test/java/com/example/missingpets/formats/DateFormatTest.java
+++ app/src/test/java/com/example/missingpets/formats/DateFormatTest.java
@@ -7,19 +7,19 @@ import org.junit.Test;
 public class DateFormatTest {
 
     @Test
-    public void yyyymmddToddmmyyy() {
+    public void ddmmyyyToyyyymmdd() {
         String datestring = "24-11-2021";
         String expectedResult = "2021-11-24";
-        String result = DateFormat.ddmmyyyToyyyymmdd(datestring);
+        String result = DateFormat.ddmmyyyyToyyyymmdd(datestring);
 
         assertEquals(result, expectedResult);
     }
 
     @Test
-    public void ddmmyyyToyyyymmdd() {
+    public void yyyymmddToddmmyyyy() {
         String datestring = "2021-11-24";
         String expectedResult = "24-11-2021";
-        String result = DateFormat.yyyymmddToddmmyyy(datestring);
+        String result = DateFormat.yyyymmddToddmmyyyy(datestring);
 
         assertEquals(result, expectedResult);
     }
